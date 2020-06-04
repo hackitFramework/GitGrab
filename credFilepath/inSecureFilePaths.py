27cd760792110cf2e9c0c11a94a582b0849f7703
Henry Samuelson <hsamuelson@users.noreply.github.com>
2020-06-04 01:57:29 -0700

    regex update

--- credFilepath/inSecureFilePaths.py
+++ credFilepath/inSecureFilePaths.py
@@ -21,9 +21,9 @@ def parse(filename):
         line_num += 1
         line = file.readline().strip()
         if line.split() == []: break
-        query = re.findall(r"[\/.][a-zA-Z0-9\s]{1,100}[\/.]",line) #/something/
-        query.append(re.findall(r"[\/.][a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}[\/.]",line)) #/something/something
-        query.append(re.findall(r"[\/.][a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}[\/.]",line)) # /something/something/something
+        query = re.findall(r"[\/.][a-zA-Z0-9\s]{1,100}(\"|/|$)",line) #/something/
+        query.append(re.findall(r"[\/.][a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}(\"|/|$)",line)) #/something/something
+        query.append(re.findall(r"[\/.][a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}(\"|/|$)",line)) # /something/something/something
 
 
         line_valid = False
@@ -57,11 +57,11 @@ def poke_directory(lines):
     for i in range(0, len(lines)):
         line = lines[i]
         # this is inexusable code, I recognize this is horrid in practice.
-        query = re.findall(r"[\/.][a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}[\/.]",line) # /something/something/something
+        query = re.findall(r"[\/.][a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}(\"|/|$)",line) # /something/something/something
         if query == []:
-            query = re.findall(r"[\/.][a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}[\/.]",line) #/something/something
+            query = re.findall(r"[\/.][a-zA-Z0-9\s\-]{1,100}\/[a-zA-Z0-9\s\-]{1,100}(\"|/|$)",line) #/something/something
             if query == []:
-                query = re.findall(r"[\/.][a-zA-Z0-9\s]{1,100}[\/.]",line) #/something/
+                query = re.findall(r"[\/.][a-zA-Z0-9\s]{1,100}(\"|/|$)",line) #/something/
             if(query != []):
                 direc = query[0] 
                 direc = "cd " + direc
