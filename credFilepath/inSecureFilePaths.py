4ab8a0359d728ed8664a5deb5c7eddb29c95f047
Henry Samuelson <hsamuelson@users.noreply.github.com>
2020-07-21 01:42:31 -0400

    updates

--- credFilepath/inSecureFilePaths.py
+++ credFilepath/inSecureFilePaths.py
@@ -1,10 +1,18 @@
 #
-# Insecure File-path data sink
+# Stored Credential Harvester
 #
 # Henry Samuelson 6/2/2020
-
+#
+# The idea here is this script reads through  plaintext files, looking for varaibles that
+# look like passwords and are assigned from filepaths.
+# The script then trys to access the filepaths with its user premissions; if the script can
+# get access to the password file, then you could also have access. The script then returns
+# the filepath of the password and it returns the line of the source file the reference was 
+# found on.
+#** This is still test code
 import re
 import os
+import sys
 
 def poke_dir(file_path,ln):
     fp = "cat" + file_path[0] + " > /dev/null 2>&1"
@@ -20,8 +28,15 @@ def parse_file(file):
         l_num+=1
         line = f.readline().strip()
         if line.split() == []: break
-        q = re.findall(r'\/.*\.[\w:]+'. str(line))
+        q = re.findall(r'\/.*\.[\w:]+', str(line))
         if q != []:
             q1 = re.findall(r"[Pp]assword=|pwd=|PWD=|[Pp]ass=|[Cc]red=|[Cc]reds=|[Cc]redential=|[Cc]redentials=|[Aa]dmin=|passwrd=|[Kk]ey=|[Kk]eys=|private.*[Kk]ey$=",line)
             q2 = re.findall(r"[Pp]assword =|pwd =|PWD =|[Pp]ass =|[Cc]red =|[Cc]reds =|[Cc]redential =|[Cc]redentials =|[Aa]dmin =|passwrd =|[Kk]ey =|[Kk]eys =|private.*[Kk]ey$ =",line)
             if (q1 != [] and q1 != ['"']) | (q2 != [] and q2 != ['"']): insecure_ref.append(poke_dir(q,l_num))
+
+os.system("clear")
+print("")
+print("     --> StoredCredential Harvester <--")
+print("~H.E.Samuelson")
+print("")
+parse_file(sys.argv[0])
--- credFilepath/insecure.sh
+++ credFilepath/insecure.sh
@@ -3,3 +3,4 @@ password= -s "/root"
 shell butt
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 pwd= -s "/usr/bin"
+keys = /root/test.txt
